5a97a851e3abcf5e535ff29ef2a255675ee398a3
HemanthBabu21 <169127312+HemanthBabu21@users.noreply.github.com>
2025-06-25 20:35:52 +0530

    Create ring.v

---
+++ ring_counter/ring.v
@@ -0,0 +1,14 @@
+module ring(
+    input clk,
+    input rst,
+    output reg [3:0] out
+    );
+    
+always@(posedge clk)
+begin
+    if(rst)
+        out<=4'b0001;
+    else
+        out<={out[2:0],out[3]};
+end
+endmodule
