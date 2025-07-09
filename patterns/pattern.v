4c38f4f944b9ca93a556bae34d0843c41bc6e052
HemanthBabu21 <169127312+HemanthBabu21@users.noreply.github.com>
2025-07-09 11:27:41 +0530

    Create pattern.v

---
+++ patterns/pattern.v
@@ -0,0 +1,17 @@
+module pattern(
+
+    );
+integer i,j;
+initial
+begin
+for(i=1;i<=5;i=i+1)
+    begin
+        for(j=1;j<=i;j=j+1)
+            begin
+            $write("",j);
+            end
+  $display("");
+  end
+   
+ end   
+endmodule
